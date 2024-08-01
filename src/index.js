b2028c41b3d8dfd843718705c5ee7281539caa9b
domager73 <domager73@gmail.com>
2024-08-01 22:52:34 +0300

    ADDED: - starter pak

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Provider} from "react-redux";
-import {initStore} from "./store/Store";
+import {initStore} from "./Store/Store";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +13,7 @@ root.render(
         <BrowserRouter>
             <Routes>
                 {/* подстановочный путь */}
-                <Route path="main" element={<App/>}/>
+                <Route path="*" element={<App/>}/>
             </Routes>
         </BrowserRouter>
     </Provider>
