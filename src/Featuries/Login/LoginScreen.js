2ede59d0d624b41a7900d581751e45f7c5f4d709
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-08-02 14:19:25 +0300

    les go

--- src/Featuries/Login/LoginScreen.js
+++ src/Featuries/Login/LoginScreen.js
@@ -1,11 +1,12 @@
 import styles from './LoginScreen.module.css'
-// // import fonts from './Utils/AppTypography.module.css'
-// import colors from 'src/Utils/AppColors.module.css'
+import fonts from '../../Utils/AppTypography.module.css'
+import colors from '../../Utils/AppColors.module.css'
 import classNames from "classnames";
 
 function LoginScreen() {
 
     return <div className={styles.login}>
+        <p className={classNames(fonts.f24w800, colors.white)}>123123</p>
     </div>
 }
 
